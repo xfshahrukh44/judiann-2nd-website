ef5d5d37c90827a8c35a38e5f16a784ed0c4a87b
Shahrukh Siddiqui <shahrukh.siddiqui@callcenter.com>
2022-09-13 16:26:55 +0500

    customer portal: registered courses orderBy and owned sessions

--- app/Http/Controllers/Customer/CourseSessionController.php
+++ app/Http/Controllers/Customer/CourseSessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use App\Models\CourseSession;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CourseSessionController extends Controller
 {
@@ -12,7 +13,11 @@ class CourseSessionController extends Controller
     {
         try {
             if (request()->ajax()) {
-                return datatables()->of(CourseSession::with('course')->whereHas('course')->get())
+                return datatables()->of(CourseSession::with('course')
+                    ->where('user_id', Auth::id())
+                    ->whereHas('course')
+                    ->orderBy('created_at', 'DESC')
+                    ->get())
                     ->addIndexColumn()
                     ->addColumn('action', function ($data) {
                         return '<a title="View" href="course-session-view/' . $data->id . '" class="btn btn-dark btn-sm"><i class="fas fa-eye"></i></a>';
